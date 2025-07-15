122524cb6cf81ee494edbc7a04175c23d3839fe8
Ashish Kumar <agnihotri.ashishkumar25@gmail.com>
2025-07-16 01:23:07 +0530

    card component

---
+++ src/Components/Card.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import List from "./LIst";
+import '../App.css'
+
+function Cards() {
+  return (
+    <>
+      <div className="card">
+        <h1>Card Components</h1>
+        <List />
+      </div>
+    </>
+  );
+}
+
+export default Cards;
---
+++ src/Components/List.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const List = () => {
+  return (
+    <>
+      <ul>
+        <li>JavaScript</li>
+        <li>CSS</li>
+        <li>React</li>
+        <li>TypeScript</li>
+      </ul>
+    </>
+  );
+};
+
+export default List
