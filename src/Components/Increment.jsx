b2608d1f87c23854f1b69bff103d76ef0524aa92
Ashish Kumar <agnihotri.ashishkumar25@gmail.com>
2025-07-23 23:11:46 +0530

    input in form & event

--- src/Components/Increment.jsx
+++ src/Components/Increment.jsx
@@ -1,15 +1,24 @@
 import { useState } from "react";
 
 const Increment= () =>{
+    {/* useState returns an array */}
     const [count, setCount] = useState(0)
     const handleClick = ()=>{
         setCount(count + 1);
 
     }
 
+    const [input, setInput] = useState('')
+  const inputHandler  = (event) =>{
+    setInput(event.target.value)
+
+  }
+
     return <>
     <h2>{count}</h2>
+    <input type="text" onChange={inputHandler}></input>
     <button onClick={handleClick}>Click</button>
+    <h3>{input}</h3>
 
     </>
 
