8deebcac5cb4cebaf427c4b18fcd0220822f4e4f
Ashish Kumar <agnihotri.ashishkumar25@gmail.com>
2025-07-23 21:23:53 +0530

    useState Hook

--- src/Components/Increment.jsx
+++ src/Components/Increment.jsx
@@ -1,12 +1,14 @@
+import { useState } from "react";
+
 const Increment= () =>{
-    let task = 0;
-    const handleClick = (e)=>{
-        task++;
-        console.log("button clicked.!!", task)
+    const [count, setCount] = useState(0)
+    const handleClick = ()=>{
+        setCount(count + 1);
 
     }
 
     return <>
+    <h2>{count}</h2>
     <button onClick={handleClick}>Click</button>
 
     </>
